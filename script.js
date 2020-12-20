916672c39764b6206233a5710c94ab5082977cae
Peter Ting <pting1995@gmail.com>
2020-12-20 15:27:18 -0800

    added a timer and a gameover screen if that timer hits 0

--- script.js
+++ script.js
@@ -6,10 +6,57 @@
 // <!-- when all questions are answered you can save initials and score -->
 
 // when start button is pressed change h1 to quiz question, hide start button and show possible answers
-var startBtn = document.getElementById("start")
-var h1 = document.getElementById("quiz-text")
 
+var timerEl = document.getElementById("time-left");
+
+var cardBody = document.getElementById("card-body");
+
+var h1 = document.getElementById("quiz-text");
+
+var desc = document.getElementById("description");
+
+var startBtn = document.getElementById("start");
+
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
 
 startBtn.addEventListener("click", function() {
-    h1.textContent = "Question One"
+    
+    secLeft();
+    questionOne();
 })
+
+function secLeft() {
+    var timeLeft = 5;
+    setInterval(function() {
+        timerEl.textContent = timeLeft + " seconds left";
+        timeLeft--;
+
+        if (timeLeft === 0) {
+            timerEl.textContent = "";
+            clearInterval(secLeft);
+            gameOver();
+        }
+    }, 1000)
+}
+
+function gameOver() {
+    h1.textContent = "u lose"
+}
+
+function questionOne() {
+    h1.textContent = "Question One"
+
+    cardBody.appendChild(button)
+}
+
+// when start button is pressed
+    // change h1 tag to question 1
+    // hide article and start button
+    // display 4 answers
+
+    // if correct answer is selected then continue
+    // else -10 sec & continue
+
+    // possible ways: inc question # by 1 when answered and create a new function per question
+    // put questions in 1 big function
