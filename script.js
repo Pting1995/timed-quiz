6c3652c8ee055affc20626586f22285d3af4c3a0
Peter Ting <pting1995@gmail.com>
2020-12-21 19:26:37 -0800

    added semantic html, added comments to help navigate code, removed redundant code, added question 3

--- script.js
+++ script.js
@@ -51,21 +51,25 @@ function secLeft() {
     }, 1000)
 }
 
+// subtracts time for wrong answers
 function minusTime() {
     timeLeft = timeLeft - 10;
     var line = document.createElement("hr");
 }
 
+// shows game over screen if user run out of time
 function gameOver() {
     h1El.textContent = "u lose"
 }
 
+// shows feedback if question is answered correctly
 function posFeedback() {
     hrEl.hidden = false
     feedbackText.textContent = "Correct!"
     feedbackText.hidden = false
 }
 
+// shows feedback if question is answered incorrectly
 function negFeedback() {
     hrEl.hidden = false
     feedbackText.hidden = false
@@ -112,9 +116,6 @@ function questionOne() {
 
 function questionTwo() {
     h1El.textContent = "What do emperor penguins eat?"
-    // hides test description and start button
-    descEl.textContent = ""
-    startBtn.style.display = "none"
 
     answer1.innerHTML = "Seaweed"
     questionArea.appendChild(answer1)
@@ -149,10 +150,44 @@ function questionTwo() {
     })
 }
 
+function questionThree() {
+    h1El.textContent = "What best describes a zebra's diet?"
 
-// create a invis div in html, change content on neg feedback and display after first question and update after
+    answer1.innerHTML = "Omnivore"
+    questionArea.appendChild(answer1)
+    answer2.innerHTML = "Carnivore"
+    questionArea.appendChild(answer2)
+    answer3.innerHTML = "Herbivore"
+    questionArea.appendChild(answer3)
+    answer4.innerHTML = "Cannibal"
+    questionArea.appendChild(answer4) 
 
-    
+    answer1.addEventListener("click", function() {
+        minusTime()
+        highScore()
+        negFeedback()
+    })
+
+    answer2.addEventListener("click", function() {
+        minusTime()
+        highScore()
+        negFeedback()
+    })
+
+    answer3.addEventListener("click", function() {
+        highScore()
+        posFeedback()
+    })
+
+    answer4.addEventListener("click", function() {
+        minusTime()
+        highScore()
+        negFeedback()
+    })
+}
+
+// if Highscores button is pressed, or if question 3 is answered then show highscores
+
 // when start button is pressed
     // change h1 tag to question 1
     // hide article and start button
